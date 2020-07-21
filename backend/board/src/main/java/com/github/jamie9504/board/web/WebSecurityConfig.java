877fd91547396be8010bcb19b597bf9033b8eba5
jamie9504 <jamie9504@kakao.com>
2020-07-21 23:05:11 +0900

    feat: Spring-Security Sample 코드 작성

---
+++ backend/board/src/main/java/com/github/jamie9504/board/web/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.github.jamie9504.board.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+            User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+        return manager;
+    }
+}
