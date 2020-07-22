67984e0ebc990dfcf1d2a6b8aa2ce2c636adb968
jamie9504 <jamie9504@kakao.com>
2020-07-22 16:06:38 +0900

    refactor: 초기 개발시 Login하지 않아도 접근 되도록 설정
    - SpringSecurity 설정 조정
    - CRLF 이슈 발생으로 disable() - 추후 변경

--- backend/board/src/main/java/com/github/jamie9504/board/web/WebSecurityConfig.java
+++ backend/board/src/main/java/com/github/jamie9504/board/web/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.github.jamie9504.board.web;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +11,20 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO: 22/07/2020 csrf.disable 추후 제거
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/**").permitAll();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
