559667d3f9eb5d77835878c10d6571aa8f6f4300
jamie9504 <jamie9504@kakao.com>
2020-08-04 23:16:33 +0900

    feat: User 관련 Controller 설정
    - https://velog.io/@minholee_93/Spring-Security-Database-Authentication-Spring-Boot-6 참고

--- backend/board/src/main/java/com/github/jamie9504/board/web/ApiController.java
+++ backend/board/src/main/java/com/github/jamie9504/board/web/ApiController.java
@@ -1,5 +1,9 @@
 package com.github.jamie9504.board.web;
 
+import com.github.jamie9504.board.user.User;
+import com.github.jamie9504.board.user.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/public")
+@RequiredArgsConstructor
 public class ApiController {
+
+    private final UserRepository userRepository;
 
     @GetMapping("test1")
     public ResponseEntity<String> getTest1() {
@@ -18,5 +25,9 @@ public class ApiController {
     public ResponseEntity<String> getTest2() {
         return ResponseEntity.ok("API Test 2");
     }
-}
 
+    @GetMapping("users")
+    public List<User> allUsers() {
+        return this.userRepository.findAll();
+    }
+}
