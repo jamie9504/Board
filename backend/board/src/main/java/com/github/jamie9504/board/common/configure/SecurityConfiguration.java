c993e7ab352a68ea2668d3ed6edaaeccd9e140be
jamie9504 <jamie9504@kakao.com>
2020-08-04 23:19:12 +0900

    feat: logout 화면 버튼 추가 및 기능 추가

--- backend/board/src/main/java/com/github/jamie9504/board/common/configure/SecurityConfiguration.java
+++ backend/board/src/main/java/com/github/jamie9504/board/common/configure/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -54,6 +55,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/public/users").hasRole("ADMIN")
             .and()
             .formLogin()
-            .loginPage("/login").permitAll();
+            .loginPage("/login").permitAll()
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login");
     }
 }
