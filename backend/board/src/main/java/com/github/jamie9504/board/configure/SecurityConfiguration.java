be509902aebba18fdb87d8225a146f8332bdf363
jamie9504 <jamie9504@kakao.com>
2020-08-04 23:04:41 +0900

    feat: SecurityConfiguration 구현
    - @EnableWebSecurity을 사용해 Security에서 사용할 설정 명시
    - custom security 설정을 구현하기 위해 WebSecurityConfigurerAdapter 상속 후 Override
    - https://velog.io/@minholee_93/Spring-Security-HTTP-Basic-Authentication-Spring-Boot-2 참고

---
+++ backend/board/src/main/java/com/github/jamie9504/board/configure/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.github.jamie9504.board.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN")
+            .and()
+            .withUser("user")
+            .password(passwordEncoder().encode("user"))
+            .roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        web.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
