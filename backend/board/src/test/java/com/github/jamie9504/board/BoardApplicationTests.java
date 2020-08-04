52df6ddfd77ce0dd410947d9ce076f5ee40a9c0e
jamie9504 <jamie9504@kakao.com>
2020-08-05 00:01:12 +0900

    test: RandomPort 제거

--- backend/board/src/test/java/com/github/jamie9504/board/BoardApplicationTests.java
+++ backend/board/src/test/java/com/github/jamie9504/board/BoardApplicationTests.java
@@ -2,13 +2,11 @@ package com.github.jamie9504.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class BoardApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
